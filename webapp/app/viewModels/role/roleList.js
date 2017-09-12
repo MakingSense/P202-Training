29ed691fdddf366aeec9a7c9cec5cf83660fbd54
Luigi Alcabelu <alcabelu@gmail.com>
2017-09-12 07:20:56 -0500

    Add entity Rol, for display a row in a grid of roles

--- webapp/app/viewModels/role/roleList.js
+++ webapp/app/viewModels/role/roleList.js
@@ -1,6 +1,17 @@
 require('services/roleService');
 
 define(['services/roleService'], function (roleService) {
+
+
+    // Class to represent a row in the seat reservations grid
+    function rol(id,name,createdby) {
+        var self = this;
+        self.name = name;
+        self.createdby = createdby;
+        self.id = id;
+        
+    }
+
   var vm = {
     getView: function () {
       var view = require('./roleList.html');
