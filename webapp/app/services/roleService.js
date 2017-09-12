298479e48d799e886e42bdbc05d01998c9217926
Luigi Alcabelu <alcabelu@gmail.com>
2017-09-12 11:24:11 -0500

    generate a simple object for rolinsert,
    eliminate the console log statments

--- webapp/app/services/roleService.js
+++ webapp/app/services/roleService.js
@@ -1,13 +1,6 @@
-var rolInsert = function (name) {
-    this.Name = name;
-};
-
 define('services/roleService', [], function () {
 
-
-
     function roleReadAll() {
-        // TODO: refactor common headers and base url
         console.log('Entre a llamar WebService')
         return $.ajax({
             type: 'POST',
@@ -18,26 +11,15 @@ define('services/roleService', [], function () {
     }
 
     function roleAdd(value) {
-        // TODO: refactor common headers and base url
-
-        console.log('dentro de roleAdd de roleservices');
-        console.log('valor de rolesservices');
-        console.log(value);
-        console.log('valor Rol');
-        var RoleInsert = new rolInsert(value);
-        console.log(RoleInsert);
-        console.log('valor Rol');
-        var RoleInsertJ = JSON.stringify(RoleInsert);
-
-        console.log('valor Rol JSON');
-        console.log(RoleInsertJ);
-
-        console.log('Ejecuto Ajax');
+        var roleIns = {
+            "Name":value
+        };              
+        var roleInsJs = JSON.stringify(roleIns);
         return $.ajax({
             type: 'POST',
             contentType: 'application/json',
             url: 'http://localhost:10160/Service.svc/jsonp/post',
-            data: '{"requests":[{"__type":"RoleAddRequest:#P202.Training.WCF.RequestsAndResponses","Role":' + RoleInsertJ + '}]}'
+            data: '{"requests":[{"__type":"RoleAddRequest:#P202.Training.WCF.RequestsAndResponses","Role":' + roleInsJs + '}]}'
         });
     }
 
--- webapp/app/viewModels/role/RoleAdd.js
+++ webapp/app/viewModels/role/RoleAdd.js
@@ -1,37 +1,18 @@
 require('services/roleService');
 
 define('Echo', ['services/roleService'], function (roleService) {
-
-
-    var rolInsert = function (name) {
-        this.Name = name;
-    };    
-
+    
     var vm = {
         getView: function () {
             var view = require('./roleAdd.html');
             return $.parseHTML(view)[0];
         },
         name: ko.observable(),
-
         roleAdd: roleAdd,
         roleResponse: ko.observable('')
     };
 
     function roleAdd() {
-
-        console.log('entre a RoleAdd');        
-        console.log(this.name());
-        console.log('valor de name upp');
-
-        var RolInsert = new rolInsert(this.name());
-        
-        console.log('Objeto Rol Insert');
-        console.log(RolInsert);
-        
-       
-       
-        console.log('Voy a llamar la funcion dice role undefined');
         roleService.roleAdd(this.name()).then(function (response) {
             console.log('dentro');
             vm.roleResponse(response.ProcessJsonRequestsPostResult[0].Value);
