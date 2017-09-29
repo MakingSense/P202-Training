3c3d9029a300b7f13018a04a9eb23c416b910a42
Ignacio Joakin <ijoakin@gmail.com>
2017-09-29 11:42:46 -0300

    ToDoItems, toDoLists create Tests

--- P202.Training.Test/ToDoListTest.cs
+++ P202.Training.Test/ToDoListTest.cs
@@ -1,19 +1,60 @@
 ﻿using AutoMapper;
 using Moq;
+using P202.Training.Data.Entities;
 using P202.Training.Data.Repositories;
 using P202.Training.Domain;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace P202.Training.Test
 {
-    
+
     public class ToDoListTest
     {
+        [Fact]
+        public void CreateToDoList_PersistsChange()
+        {
+            // Arrange
+            var ToDoListRepositoryMock = new Mock<IToDoListRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            var modelToDoList = new Domain.Models.ToDoList
+            {
+                Id = 1,
+                Name = "ToDoList A"
+            };
+
+            var entityToDoList = new Data.Entities.ToDoList
+            {
+                Id = 1,
+                Name = "ToDoList B"
+            };
+
+            ToDoListRepositoryMock.Setup(r => r.Create(It.IsAny<ToDoList>()));
+            ToDoListRepositoryMock.Setup(r => r.FindById(It.IsAny<int>())).Returns(entityToDoList);
+
+            mapperMock.Setup(m => m.Map<Domain.Models.ToDoList, Data.Entities.ToDoList>(It.IsAny<Domain.Models.ToDoList>()))
+                            .Returns(entityToDoList);
+            mapperMock.Setup(m => m.Map<Data.Entities.ToDoList, Domain.Models.ToDoList>(It.IsAny<Data.Entities.ToDoList>()))
+                .Returns(modelToDoList);
+
+            // Act
+            var ToDoListRepositoryMockObject = ToDoListRepositoryMock.Object;
+            var mappingServiceMockObject = mapperMock.Object;
+
+            var ToDoListService = new ToDoListService(ToDoListRepositoryMockObject, mappingServiceMockObject);
+            ToDoListService.CreateToDoList(modelToDoList);
+
+            //Assert that the Add method was called once
+            ToDoListRepositoryMock.Verify(x => x.Create(It.IsAny<Data.Entities.ToDoList>()), Times.Once());
+
+            var expectedToDoList = ToDoListService.ReadToDoList(1);
+
+            // Assert
+            Assert.Equal(modelToDoList.Name, expectedToDoList.Name);
+        }
+
+
         [Fact]
         public void GetToDoList_ListData()
         {
@@ -47,8 +88,8 @@ namespace P202.Training.Test
             var toDoListRepositoryMockObject = toDoListRepositoryMock.Object;
 
             // Act
-            var toDoItemService = new ToDoListService(toDoListRepositoryMockObject, mappingServiceMockObject);
-            var expectedToDoList = toDoItemService.ListToDoList();
+            var ToDoListService = new ToDoListService(toDoListRepositoryMockObject, mappingServiceMockObject);
+            var expectedToDoList = ToDoListService.ListToDoList();
 
             //Assert that the List method was called once
             toDoListRepositoryMock.Verify(x => x.FindAll(), Times.Once());
